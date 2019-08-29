c859a42d0c42f3bf1edec94d684c18912c6c5b52
Bren <brennenbates2859@gmail.com>
2019-08-28 23:00:25 -0600

    Update game.js
    Adding comments to the code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
+//Programmer: Brennen Bates
+//Date: 8/28/2019
+
+////////////
+//Preface://
+////////////
+
+//I chose to set up the movie options as objects with attributes for the picture, sound and the text that will display under the header at the top.
+
 
 var Initial= {
     "movieText" : "Can you guess the movie?",
@@ -56,26 +65,38 @@ var Victory = {
 
 
 
+///////////////
+//Main Process/
+///////////////
 
-//Main Process
-//==========================================================================================
-   
-   // Set-up necessary variables for wins counter, movies index, number of guesses etc.
-//==========================================================================================
+//======================================================================================//
+//   Set-up necessary variables for wins counter, movies index, number of guesses etc.  //
+//======================================================================================//
+
+//Set the movie objects above into an array so that they can be cycled through later. 
 
 var Movies = [Initial, Braveheart, Deadpool, Titanic, Jaws, Starwars];
 var wins= 0;
+//I set the movie index to 1 inidially, bedause I stored the initial webpage settings in the 0 index.
 var movieIndex = 1;
+
+//I set up variable x as the number of guesses remaining.  I used x because I used 'guesses remaining' as the id for the html document.
 var x = 15;
+
+//Game over variable equal to false.  I set my game up so that if they run out of remaining guesses they get a prompt to push 'r' to restart the game.  
+//They can do that in the end of the game too if they win.
 var game_over = false;
 
-//Current word is the word on the screen that will change as they are guessing letters. For loop to first clear out the currentWord of the last word variable and then to push underscores to it for the length of the new word.
+//Current word is the word on the screen that will change as they are guessing letters. 
+//For loop to first clear out the currentWord of the last word variable and then to push underscores to it for the length of the new word.
 var currentWordMatrix = [];
 
 //Create an array to store the guessed letters. 
 var lettersGuessedMatrix = [];
 
-//Make an array containing all of the audio.
+//Make an array containing all of the audio.   This made it easier for me to stop the music from the prior movie and start the current movie music so 
+//I didn't have overlap of sounds. 
+
 var audio = [];
 audio.length = Movies.length;
 
@@ -83,7 +104,7 @@ for (var k = 0; k < Movies.length; k++) {
     audio[k] = new Audio(Movies[k].movieSound);
 }
 
-//Play initial sound.
+//Play initial sound.  I set it up to play the 20th century fox sound when the web page is opened. 
 audio[0].play();
 
 
@@ -92,70 +113,121 @@ audio[0].play();
 
   
 //Set up the movie text, picture and sound to be for the last word that they just guessed.
-    document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
-    document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
+
+document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
+document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
 
 
+for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
+    var blank = "_";
+    currentWordMatrix.push(blank);}
 
-   var blank;
+//Join the currentWordMatrix entries together to make the currentWord string.  Also, make it lower case.
+var currentWord = currentWordMatrix.join('');
+var solutionWord = Movies[movieIndex].movieName;
 
-       for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
-           blank = "_";
-           currentWordMatrix.push(blank);
-       }
+//Push the current word to the screen
+document.querySelector("#currentWord").innerHTML = currentWord;
 
-    //Join the currentWordMatrix entries together to make the currentWord string.  Also, make it lower case.
-   var currentWord = currentWordMatrix.join('');
-
-   var solutionWord = Movies[movieIndex].movieName;
-
-
-   //Push the current word to the screen
-   document.querySelector("#currentWord").innerHTML = currentWord;
-
-   //Start up the game 
-   runGame();
+//Start up the game 
+runGame();
       
 
 // Functions 
 // ========================================================================================
 
 
+function runGame() {
+     
+    //When user pushes a key, perform various functions. 
+ 
+     document.onkeydown = function(event) {
+     
+         //Converting the keyinput to lower case.  The game won't include upper case letters.
+ 
+             var keyInput = event.key.toLowerCase(); 
+           
+            document.querySelector("#guessLetter").innerHTML = keyInput
+           
+            document.querySelector("#guessesRemaining").innerHTML = x;
+ 
+            document.querySelector("#wins").innerHTML = wins;
+ 
+ 
+            //Check if the guessed letters matrix includes the key pushed, if not add it to the list.  I wanted the letters to be comma delimited for readability.
+            
+            if (lettersGuessedMatrix.includes(keyInput + ', ') === false) {
+                lettersGuessedMatrix.push(keyInput + ', ');
+             //reduce number of guesses remaining.
+             x = x-1; 
+            }
+ 
+            var Guesses = lettersGuessedMatrix.join('');
+            console.log(Guesses);
+
+           document.querySelector("#guessedLetters").innerHTML = Guesses;
+              
+ 
+            //If the user guesses a letter that is contained in the solution word, then add it to the current word.  
+            //Needed to cycle through the solution and compare what they guessed to each letter of the solution.  If they guessed a letter of the solution word, add it to 
+            //the 'current word' that they are filling out.   Used the substring function to piece together the new current word with their correct key guess added in.
+
+            for (var j=0; j < currentWord.length;j++) {
+ 
+                if (solutionWord.charAt(j) === keyInput) {
+                 newWord = currentWord.substring(0,(j)) + keyInput + currentWord.substring((j+1),length.currentWord);
+                 currentWord = newWord;
+                 document.querySelector("#currentWord").innerHTML = currentWord;
+                } 
+            }
+ 
+             //reduce the allowable guesses count for each key strike.  If user goes below 0 it's a game over.
+        
+             if(x <= 0) {
+                 audio[movieIndex].pause();
+                 audio[movieIndex].currentTime = 0;
+                 gameOver()
+             }
+            
+     
+             //Completely guessing a word will set up the next movie and increase the win count. 
+            
+         
+             if(currentWord === solutionWord && movieIndex !== Movies.length && game_over === false) {
+                 nextMovie()  
+             }
+ 
+             if(currentWord === solutionWord && movieIndex === Movies.length) {
+                 youWin()  
+              }
+
+         }
+ 
+ 
+ }
 
 function reset() {
 
-    // Set-up necessary variables for wins counter, movies index, number of guesses etc.
+    // Reset necessary variables for wins counter, movies index, number of guesses etc.
 //==========================================================================================
 
-
-
-Movies = [Initial, Braveheart, Deadpool, Titanic, Jaws];
 wins= 0;
 movieIndex = 1;
 x = 15;
 game_over = false;
 
-//Current word is the word on the screen that will change as they are guessing letters. For loop to first clear out the currentWord of the last word variable and then to push underscores to it for the length of the new word.
+//Reset arrays.
 currentWordMatrix = [];
-
-//Create an array to store the guessed letters. 
 lettersGuessedMatrix = [];
-
-//Make an array containing all of the audio.
-audio = [];
-audio.length = Movies.length;
-
-for (var k = 0; k < Movies.length; k++) {
-    audio[k] = new Audio(Movies[k].movieSound);
-}
 
 //Play initial sound.
 audio[0].play();
   
-//Set up the movie text, picture and sound to be for the last word that they just guessed.
-    document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
-    document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
+//Reset image and text. 
+document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
+document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
 
+//Reset 'Current' and 'Solution' words. 
    var blank;
 
        for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
@@ -163,129 +235,66 @@ audio[0].play();
            currentWordMatrix.push(blank);
        }
 
-    //Join the currentWordMatrix entries together to make the currentWord string.  Also, make it lower case.
-   currentWord = currentWordMatrix.join('');
-
-   solutionWord = Movies[movieIndex].movieName;
+currentWord = currentWordMatrix.join('');
+solutionWord = Movies[movieIndex].movieName;
 
 
-   //Push the current word to the screen
-   document.querySelector("#currentWord").innerHTML = currentWord;
+//Push the current word to the screen
+document.querySelector("#currentWord").innerHTML = currentWord;
 
-   runGame();
-}
-
-function runGame() {
-     
-   //When user pushes a key, perform various functions. 
-
-    document.onkeydown = function(event) {
-    
-        //Converting the keyinput to lower case.  The game won't include upper case letters.
-
-            var keyInput = event.key.toLowerCase(); 
-          
-           document.querySelector("#guessLetter").innerHTML = keyInput
-          
-           document.querySelector("#guessesRemaining").innerHTML = x;
-
-           document.querySelector("#wins").innerHTML = wins;
-
-
-           //Check if the guessed letters matrix includes the key pushed, if not add it to the list. 
-           
-           if (lettersGuessedMatrix.includes(keyInput + ', ') === false) {
-               lettersGuessedMatrix.push(keyInput + ', ');
-            //reduce number of guesses remaining.
-            x = x-1; 
-           
-           }
-
-           var Guesses = lettersGuessedMatrix.join('');
-           console.log(Guesses);
-          document.querySelector("#guessedLetters").innerHTML = Guesses;
-             
-
-           //If the user guesses a letter that is contained in the solution word, then add it to the current word.
-           for (var j=0; j < currentWord.length;j++) {
-
-            if (solutionWord.charAt(j) === keyInput) {
-                newWord = currentWord.substring(0,(j)) + keyInput + currentWord.substring((j+1),length.currentWord);
-                currentWord = newWord;
-                document.querySelector("#currentWord").innerHTML = currentWord;
-            } 
-        }
-
-            //reduce the allowable guesses count.  If user goes below 0 it's a game over.
-       
-            if(x <= 0) {
-                audio[movieIndex].pause();
-                audio[movieIndex].currentTime = 0;
-                gameOver()
-            }
-           
-    
-            //Completely guessing a word will set up the next movie and increase the win count. 
-           
-        
-            if(currentWord === solutionWord && movieIndex !== Movies.length && game_over === false) {
-                
-                nextMovie()  
-
-            }
-
-            if(currentWord === solutionWord && movieIndex === Movies.length) {
-                youWin()  
-             }
-          
-
-
-        }
-
-
+//Run game again now that everything is setup.
+runGame();
 }
 
 function gameOver() {
    
-    audio[movieIndex-1].pause();
-    audio[movieIndex-1].currentTime = 0;
-    var gameovermusic = new Audio(GameOver.Sound);
-    gameovermusic.play();
-    game_over = true;
-    document.querySelector("#movieImage").innerHTML= '<img src= ' + GameOver.Picture + '>';
-    document.querySelector("#movieText").innerHTML = GameOver.Text + " You got " + wins + " out of " + (Movies.length-1) + " movies!  Press 'r' to restart";
-    document.querySelector("#currentWord").innerHTML = 'Game is Over';
+//Stop last sound from playing
+audio[movieIndex-1].pause();
+audio[movieIndex-1].currentTime = 0;
 
-    document.onkeydown = function(event) {
+//Play sound for game over, man.  Set game_over to true to inhibit user from being able to continue to next word.
+var gameovermusic = new Audio(GameOver.Sound);
+gameovermusic.play();
+game_over = true;
+
+//Set Game Over image and text on the screen.  
+document.querySelector("#movieImage").innerHTML= '<img src= ' + GameOver.Picture + '>';
+document.querySelector("#movieText").innerHTML = GameOver.Text + " You got " + wins + " out of " + (Movies.length-1) + " movies!  Press 'r' to restart";
+document.querySelector("#currentWord").innerHTML = 'Game is Over';
+
+//Set it up so they can reset the game by hitting the letter r on their keyboard.
+
+document.onkeydown = function(event) {
        
-        //Converting the keyinput to lower case.  The game won't include upper case letters.
-
-            var keyInput = event.key.toLowerCase(); 
-            console.log(keyInput);
-            if (keyInput === 'r') {
+  var keyInput = event.key.toLowerCase(); 
+    if (keyInput === 'r') {
                 gameovermusic.pause()
                 gameovermusic.currentTime = 0;
                 reset()
             }
-        }
+    }
 }
 
 function youWin() {
 
-    
+//Stop the latest sound from running.
     audio[movieIndex-1].pause();
     audio[movieIndex-1].currentTime = 0;
+
+//Run some victory music.
     var youwinaudio = new Audio(Victory.Sound);
     youwinaudio.play();
 
+//Set game over to true so the user can't do more things.
     game_over = true;
+//Give them a nice victory picture and message.
     document.querySelector("#movieImage").innerHTML= '<img src= ' + Victory.Picture + '>';
     document.querySelector("#movieText").innerHTML = Victory.Text + " You got " + wins + " out of " + (Movies.length-1) + " movies!  Press 'r' to restart";
     document.querySelector("#currentWord").innerHTML = 'You win!';
 
+//Allow the user to restart and play the game again if they wish by hitting r on their keyboard.
+
     document.onkeydown = function(event) {
-       
-        //Converting the keyinput to lower case.  The game won't include upper case letters.
 
             var keyInput = event.key.toLowerCase(); 
             console.log(keyInput);
@@ -301,38 +310,36 @@ function youWin() {
 
 function nextMovie() {
 
-    
-    console.log('Ready for the next movie!');
-
+//Stop the last music to avoid annoying music overlay.
     audio[movieIndex-1].pause();
     audio[movieIndex-1].currentTime = 0;
     audio[movieIndex].play();
 
-    //movie index + 1, wins + 1 and reset currentWord and solutionWord
+//movie index + 1, wins + 1 and reset remaining guesses, currentWord and solution word.  
     movieIndex++;
     wins++;
-    document.querySelector("#wins").innerHTML = wins;
     x = 15;
     currentWordMatrix = [];
     lettersGuessedMatrix = [];
 
-    //Set up the movie text, picture and sound to be for the last word that they just guessed.
+//Update wins on the screen.
+    document.querySelector("#wins").innerHTML = wins;
+
+//Set up the movie text, picture and sound to be for the last word that they just guessed.
     document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
     document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
 
+//Reset the current word to be blanks so they can start guessing the next movie.
     for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
         blank = "_";
            currentWordMatrix.push(blank);
        }
-
-//Join the currentWordMatrix entries together to make the currentWord string.  Also, make it lower case.
        currentWord = currentWordMatrix.join('');
        solutionWord = Movies[movieIndex].movieName;
 
 
 //Push the current word to the screen
 document.querySelector("#currentWord").innerHTML = currentWord;
-
 
 }
 
