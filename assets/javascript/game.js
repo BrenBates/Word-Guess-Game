1d40f47b5c547056d2a9767f8eea3bc61fb7479a
Bren <brennenbates2859@gmail.com>
2019-08-29 13:16:44 -0600

    Update game.js
    Stored the initial, victory and game over objects in their own array for 'Auxiliary'.  Shuffled the Movies array so the player now gets them in a random order.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,28 @@
 //I chose to set up the movie options as objects with attributes for the picture, sound and the text that will display under the header at the top.
 
 
+//Auxilliary objects: 
+
+
 var Initial= {
-    "movieText" : "Can you guess the movie?",
-    "moviePicture" : "assets/images/movies.jpg",
-    "movieSound" : "assets/mp3/CenturyFox.mp3"
+    "Text" : "Can you guess the movie?",
+    "Picture" : "assets/images/movies.jpg",
+    "Sound" : "assets/mp3/CenturyFox.mp3"
 };
+
+var GameOver = {
+    "Text" : "Game Over!",
+    "Picture" : "assets/images/gameOver.jpg",
+    "Sound" : "assets/mp3/GameOverMan.mp3"
+};
+
+var Victory = {
+    "Text" : "You win!",
+    "Picture" : "assets/images/Victory.jpg",
+    "Sound" : "assets/mp3/SurvivorEyeoftheTiger.mp3"
+};
+
+//Movie objects:
 
 var Braveheart= {
     "movieName" : "braveheart",
@@ -51,19 +68,6 @@ var Starwars= {
     "movieSound" : "assets/mp3/Starwars.mp3"
 };
 
-var GameOver = {
-    "Text" : "Game Over!",
-    "Picture" : "assets/images/gameOver.jpg",
-    "Sound" : "assets/mp3/GameOverMan.mp3"
-};
-
-var Victory = {
-    "Text" : "You win!",
-    "Picture" : "assets/images/Victory.jpg",
-    "Sound" : "assets/mp3/SurvivorEyeoftheTiger.mp3"
-};
-
-
 
 ///////////////
 //Main Process/
@@ -73,12 +77,14 @@ var Victory = {
 //   Set-up necessary variables for wins counter, movies index, number of guesses etc.  //
 //======================================================================================//
 
-//Set the movie objects above into an array so that they can be cycled through later. 
+//Set the movie objects above into an array so that they can be cycled through later. Also, for fun I used a shuffle function on the array to pick the movie in a random order.
 
-var Movies = [Initial, Braveheart, Deadpool, Titanic, Jaws, Starwars];
+var Movies = shuffle([Braveheart, Deadpool, Titanic, Jaws, Starwars]);
+var Auxiliary = [Initial, GameOver, Victory];
+
 var wins= 0;
 //I set the movie index to 1 inidially, bedause I stored the initial webpage settings in the 0 index.
-var movieIndex = 1;
+var movieIndex = 0;
 
 //I set up variable x as the number of guesses remaining.  I used x because I used 'guesses remaining' as the id for the html document.
 var x = 15;
@@ -94,7 +100,7 @@ var currentWordMatrix = [];
 //Create an array to store the guessed letters. 
 var lettersGuessedMatrix = [];
 
-//Make an array containing all of the audio.   This made it easier for me to stop the music from the prior movie and start the current movie music so 
+//Make an array containing all of the audio for the movies and one for the auxiliary screens.   This made it easier for me to stop the music from the prior movie and start the current movie music so 
 //I didn't have overlap of sounds. 
 
 var audio = [];
@@ -104,18 +110,26 @@ for (var k = 0; k < Movies.length; k++) {
     audio[k] = new Audio(Movies[k].movieSound);
 }
 
+var audioaux = [];
+audioaux.length = Auxiliary.length;
+
+for (var m = 0; m < Auxiliary.length; m++) {
+    audioaux[m] = new Audio(Auxiliary[m].Sound);
+}
+
+
 //Play initial sound.  I set it up to play the 20th century fox sound when the web page is opened. 
-audio[0].play();
+audioaux[0].play();
 
 
 // Main Process 
 // ==========================================================================================
 
   
-//Set up the movie text, picture and sound to be for the last word that they just guessed.
+//Set up the initial web page info. 
 
-document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
-document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
+document.querySelector("#movieText").innerHTML = Auxiliary[0].Text;
+document.querySelector("#movieImage").innerHTML = '<img src= ' + Auxiliary[0].Picture + '>';
 
 
 for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
@@ -166,7 +180,7 @@ function runGame() {
             }
  
             var Guesses = lettersGuessedMatrix.join('');
-            console.log(Guesses);
+            
 
            document.querySelector("#guessedLetters").innerHTML = Guesses;
               
@@ -194,27 +208,23 @@ function runGame() {
             
      
              //Completely guessing a word will set up the next movie and increase the win count. 
+
+             console.log(wins);
+             console.log(movieIndex);
+             console.log(Movies.length);
             
-            }
 
+             if(currentWord === solutionWord && movieIndex === (Movies.length-1)) {
+                youWin()  
+             }
 
-            //On key up, evaluate if a new movie needs loaded, or to load the victory sequence.  
-
-
-             document.onkeyup = function(event) {
-         
-             if(currentWord === solutionWord && movieIndex !== Movies.length && game_over === false) {
-                 nextMovie()  
+             if(currentWord === solutionWord && movieIndex !== (Movies.length-1) && game_over === false) {
+                nextMovie() 
              }
  
-             if(currentWord === solutionWord && movieIndex === Movies.length) {
-                 youWin()  
-              }
-
+           
             }
-
-        
- 
+            
  
  }
 
@@ -224,7 +234,7 @@ function reset() {
 //==========================================================================================
 
 wins= 0;
-movieIndex = 1;
+movieIndex = 0;
 x = 15;
 game_over = false;
 
@@ -233,11 +243,12 @@ currentWordMatrix = [];
 lettersGuessedMatrix = [];
 
 //Play initial sound.
-audio[0].play();
+audioaux[0].play();
   
 //Reset image and text. 
-document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
-document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
+
+document.querySelector("#movieText").innerHTML = Auxiliary[0].Text;
+document.querySelector("#movieImage").innerHTML = '<img src= ' + Auxiliary[0].Picture + '>';
 
 //Reset 'Current' and 'Solution' words. 
    var blank;
@@ -261,17 +272,22 @@ runGame();
 function gameOver() {
    
 //Stop last sound from playing
+
+if (movieIndex > 0) {
 audio[movieIndex-1].pause();
 audio[movieIndex-1].currentTime = 0;
+}
+
+audioaux[0].pause();
+
 
 //Play sound for game over, man.  Set game_over to true to inhibit user from being able to continue to next word.
-var gameovermusic = new Audio(GameOver.Sound);
-gameovermusic.play();
+audioaux[1].play();
 game_over = true;
 
 //Set Game Over image and text on the screen.  
 document.querySelector("#movieImage").innerHTML= '<img src= ' + GameOver.Picture + '>';
-document.querySelector("#movieText").innerHTML = GameOver.Text + " You got " + wins + " out of " + (Movies.length-1) + " movies!  Press 'r' to restart";
+document.querySelector("#movieText").innerHTML = GameOver.Text + " You got " + wins + " out of " + (Movies.length) + " movies!  Press 'r' to restart";
 document.querySelector("#currentWord").innerHTML = 'Game is Over';
 
 //Set it up so they can reset the game by hitting the letter r on their keyboard.
@@ -280,8 +296,8 @@ document.onkeydown = function(event) {
        
   var keyInput = event.key.toLowerCase(); 
     if (keyInput === 'r') {
-                gameovermusic.pause()
-                gameovermusic.currentTime = 0;
+                audioaux[1].pause()
+                audioaux[1].currentTime = 0;
                 reset()
             }
     }
@@ -292,16 +308,23 @@ function youWin() {
 //Stop the latest sound from running.
     audio[movieIndex-1].pause();
     audio[movieIndex-1].currentTime = 0;
+    audioaux[0].pause();
 
 //Run some victory music.
-    var youwinaudio = new Audio(Victory.Sound);
-    youwinaudio.play();
+   
+    audioaux[2].play();
+
+//increase win count and movie index.
+wins++;
+
+// Update wins on the screen.
+document.querySelector("#wins").innerHTML = wins;
 
 //Set game over to true so the user can't do more things.
     game_over = true;
 //Give them a nice victory picture and message.
     document.querySelector("#movieImage").innerHTML= '<img src= ' + Victory.Picture + '>';
-    document.querySelector("#movieText").innerHTML = Victory.Text + " You got " + wins + " out of " + (Movies.length-1) + " movies!  Press 'r' to restart";
+    document.querySelector("#movieText").innerHTML = Victory.Text + " You got " + wins + " out of " + (Movies.length) + " movies!  Press 'r' to restart";
     document.querySelector("#currentWord").innerHTML = 'You win!';
 
 //Allow the user to restart and play the game again if they wish by hitting r on their keyboard.
@@ -309,10 +332,9 @@ function youWin() {
     document.onkeydown = function(event) {
 
             var keyInput = event.key.toLowerCase(); 
-            console.log(keyInput);
             if (keyInput === 'r') {
-                youwinaudio.pause()
-                youwinaudio.currentTime = 0;
+                audioaux[2].pause()
+                audioaux[2].currentTime = 0;
                 reset()
             }
         }
@@ -322,37 +344,76 @@ function youWin() {
 
 function nextMovie() {
 
-//Stop the last music to avoid annoying music overlay.
+
+console.log(movieIndex);
+
+//Reset remaining guesses, currentWord and solution word.  
+x = 15;
+currentWordMatrix = [];
+lettersGuessedMatrix = [];
+
+//Play new music
+audio[movieIndex].play();
+
+
+//Stop the last movie music to avoid annoying music overlay.
+
+if (movieIndex >0) {
     audio[movieIndex-1].pause();
     audio[movieIndex-1].currentTime = 0;
-    audio[movieIndex].play();
+}
 
-//movie index + 1, wins + 1 and reset remaining guesses, currentWord and solution word.  
-    movieIndex++;
-    wins++;
-    x = 15;
-    currentWordMatrix = [];
-    lettersGuessedMatrix = [];
+//Stop the initial screen music from playing if it still is.
+audioaux[0].pause();
 
-//Update wins on the screen.
-    document.querySelector("#wins").innerHTML = wins;
 
 //Set up the movie text, picture and sound to be for the last word that they just guessed.
-    document.querySelector("#movieText").innerHTML = Movies[movieIndex-1].movieText;
-    document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex-1].moviePicture + '>';
+document.querySelector("#movieText").innerHTML = Movies[movieIndex].movieText;
+document.querySelector("#movieImage").innerHTML = '<img src= ' + Movies[movieIndex].moviePicture + '>';
+
+//movie index + 1, wins + 1 
+movieIndex++;
+wins++;
+
+
+// Update wins on the screen.
+document.querySelector("#wins").innerHTML = wins;
+
 
 //Reset the current word to be blanks so they can start guessing the next movie.
+
     for (var i = 0; i < Movies[movieIndex].movieName.length; i++)  {
         blank = "_";
            currentWordMatrix.push(blank);
        }
-       currentWord = currentWordMatrix.join('');
-       solutionWord = Movies[movieIndex].movieName;
 
+currentWord = currentWordMatrix.join('');
+solutionWord = Movies[movieIndex].movieName;
+
+    
 
 //Push the current word to the screen
 document.querySelector("#currentWord").innerHTML = currentWord;
 
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
 }
 
 
